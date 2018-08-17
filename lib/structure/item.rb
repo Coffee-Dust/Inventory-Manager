fac0cee2f48daba6447ff81e61c2854b95a09c35
Jonathan Dust <d24dust@hotmail.com>
2018-08-17 14:53:52 -0400

    Fixed attributes, added creating and storing methods.

--- lib/structure/item.rb
+++ lib/structure/item.rb
@@ -1,8 +1,33 @@
 class Item
-    attr_accessor :name, :brand_name, :desc, :weight, :quantity, :sku, :sub_category
-    attr_writer :category
+    @@all = []
+    attr_accessor :name, :brand_name, :desc, :weight, :quantity, :sku, :last_received
+    attr_reader :sub_category
+
     def initialize
         #TODO: Initialize with a new SKU using SKU.new(self)
+    end
+
+    def self.create_from_hash(hash)
+        self.new.tap do |instance|
+            hash.each do |key, value|
+                begin
+                   instance.send("#{key}=", value) 
+                rescue
+                    puts "Could not find method #{key}= for #{self}"
+                end
+            end
+            instance.save
+        end
+    end
+
+    def sub_category=(sub_cat)
+        @sub_category = sub_cat
+        sub_cat.add_items(self)
+    end
+
+    def category=(cat)
+        @category = cat
+        cat.add_items(self)
     end
 
     def department
@@ -14,7 +39,19 @@ class Item
     end
 
     def category
-        @sub_category.category if @category == nil
+        if @category == nil
+            @sub_category.category
+        else
+            @category
+        end
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
     end
 
 end#endof class
