682847fe82bd2f5a02c8c5256a32e0e1b8d0ae2f
Jonathan Dust <d24dust@hotmail.com>
2018-08-17 15:01:23 -0400

    Added attributes, added creating and saving methods.

---
+++ lib/structure/sub_category.rb
@@ -0,0 +1,38 @@
+class Sub_Category
+    @@all = []
+    attr_accessor :name, :items
+    attr_reader :category
+    def initialize
+        @items  = []
+    end
+
+    def self.create_from_hash(hash)
+        self.new.tap do |instance|
+            hash.each do |key, value|
+                begin
+                   instance.send("#{key}=", value) 
+                rescue
+                    puts "Could not find method#{key}= for #{self}"
+                end
+            end
+            instance.save
+        end
+    end
+
+    def category=(cat)
+        @category = cat
+        cat.add_sub_categories(self)
+    end
+
+    def add_items(item)
+        @items << item
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
