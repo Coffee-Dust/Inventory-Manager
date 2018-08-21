242952d5d882a984930cdec25457bb61f2a3f613
Jonathan Dust <d24dust@hotmail.com>
2018-08-21 17:37:01 -0400

    Improved findable methods.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,6 @@
 module Findable
 
-    def create_from_hash(hash)
+    def new_from_hash(hash)
         self.new.tap do |instance|
             hash.each do |key, value|
                 begin
@@ -9,24 +9,17 @@ module Findable
                     puts "Could not find method#{key}= or #{self}"
                 end
             end
-            instance.save
         end
     end
 
-    def find_by_SKU(sku)
-        if self.is_a? Item.class
-            self.all.detect { |i| i.sku == sku }
-        else
-            return "ERROR: The method must be called on Items only."
-        end
+    def create_from_hash(hash)
+        instance = self.new_from_hash(hash)
+        instance.save
+        instance
     end
 
     def find_by_name(name)
         self.all.detect { |i| i.name == name }
-    end
-
-    def find_by_brand(brand)
-        self.all.detect { |i| i.brand_name == brand }
     end
 
 end#endof module
