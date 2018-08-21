2bc5fdcc6df1460f180e37997df56d5cbc77d85f
Jonathan Dust <d24dust@hotmail.com>
2018-08-21 16:23:12 -0400

    Methods functioning.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,6 @@
 module Findable
 
-    def self.create_from_hash(hash)
+    def create_from_hash(hash)
         self.new.tap do |instance|
             hash.each do |key, value|
                 begin
@@ -13,16 +13,20 @@ module Findable
         end
     end
 
-    def self.find_by_SKU(sku)
-
+    def find_by_SKU(sku)
+        if self.is_a? Item.class
+            self.all.detect { |i| i.sku == sku }
+        else
+            return "ERROR: The method must be called on Items only."
+        end
     end
 
-    def self.find_by_name(name)
-
+    def find_by_name(name)
+        self.all.detect { |i| i.name == name }
     end
 
-    def self.find_by_brand(brand)
-
+    def find_by_brand(brand)
+        self.all.detect { |i| i.brand_name == brand }
     end
 
 end#endof module
