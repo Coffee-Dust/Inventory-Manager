7073a1179f66f2fdc1c38f5513810a3b6a5a3029
Jonathan Dust <d24dust@hotmail.com>
2018-08-23 15:38:22 -0400

    Created the model for the scraper.

---
+++ lib/scraper.rb
@@ -0,0 +1,24 @@
+class Scraper
+    #TODO: Expected behaivor: Department.create_from_hash(scraper.departments)
+    attr_accessor :address, :departments, :categories, :sub_categories, :items
+    def initialize(address)
+        @address = address
+        
+    end
+
+    def get_departments
+
+    end
+
+    def get_categories
+
+    end
+
+    def get_sub_categories
+
+    end
+
+    def get_items
+
+    end
+end
