6bb6374eea9b95e4083d4a9d53d5d20794315b9c
Jonathan Dust <d24dust@hotmail.com>
2018-11-22 12:11:46 -0600

    Made methods for sku generation.

--- lib/structure/SKUmaker.rb
+++ lib/structure/SKUmaker.rb
@@ -1,5 +1,22 @@
 class SKU
-    def initialize
-        
+    @@last_item = []
+    def initialize(item)
+        @item = item
+        @value = []
+    end
+
+    def generate_SKU
+         #get the lastest/highest SKU number by searching for it.
+         @value << @item.department.name.chr
+         @value << @item.category.name.chr
+
+    end
+
+    def generate_uniq_number
+
+    end
+
+    def value
+        @value
     end
 end
