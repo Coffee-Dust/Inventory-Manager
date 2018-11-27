c61adfc4b0714d2b62bca536703df9a44982b6d3
Jonathan Dust <d24dust@hotmail.com>
2018-11-27 12:19:53 -0600

    Completed and functioning properly.

--- lib/structure/SKUmaker.rb
+++ lib/structure/SKUmaker.rb
@@ -2,21 +2,27 @@ class SKU
     @@last_item = []
     def initialize(item)
         @item = item
-        @value = []
+
+        self.generate_SKU
+        @@last_item.clear
+        @@last_item << @item
     end
 
     def generate_SKU
-         #get the lastest/highest SKU number by searching for it.
-         @value << @item.department.name.chr
-         @value << @item.category.name.chr
+        @value = []
+        @value << @item.department.name.chr
+        @value << @item.category.name.chr
+        @value << self.generate_uniq_number
+        
+        @value = @value.join
 
     end
 
     def generate_uniq_number
-        begin
-           amount = @@last_item.sku.scan(/[^0A-Za-z]/).join.to_i 
-        rescue => exception
-             amount = 0
+        if @@last_item.empty? || @@last_item[0].sku[0,2] != @value.join
+            amount = 0
+        else
+            amount = @@last_item[0].sku.scan(/[^0A-Za-z]/).join.to_i 
         end
         amount += 1
         amount  = amount.to_s.split("")
