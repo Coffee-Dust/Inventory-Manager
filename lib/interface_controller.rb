3fbe31add0d07725d2e62f3067c973906cae6df9
Jonathan Dust <d24dust@hotmail.com>
2018-12-21 16:25:21 -0500

    Added methods.

--- lib/interface_controller.rb
+++ lib/interface_controller.rb
@@ -1,5 +1,48 @@
 class Interface_Controller
-    def initialize
+    include Interface_Controller::COMMANDS
+    def initialize(manager)
+        @manager = manager
+        #start command loop.
+    end
+
+    def view_home
+        puts '
+██╗  ██╗ ██████╗ ███╗   ███╗███████╗
+██║  ██║██╔═══██╗████╗ ████║██╔════╝
+███████║██║   ██║██╔████╔██║█████╗  
+██╔══██║██║   ██║██║╚██╔╝██║██╔══╝  
+██║  ██║╚██████╔╝██║ ╚═╝ ██║███████╗
+╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝'
+
+        puts "To return to home at anytime, type \'home\'"
+
+        low_items = @manager.get_lowest_quantity
         
+        puts "\nLow Inventory: Order soon!"
+
+        puts "#{low_items[0].department.name}: #{low_items[0].name}: #{color_quantity(low_items[0].quantity)} | #{low_items[1].department.name}: #{low_items[1].name}: #{color_quantity(low_items[1].quantity)}"
+        puts "\n#{low_items[2].department.name}: #{low_items[2].name}: #{color_quantity(low_items[2].quantity)} | #{low_items[3].department.name}: #{low_items[3].name}: #{color_quantity(low_items[3].quantity)}"
+        puts "\n#{low_items[4].department.name}: #{low_items[4].name}: #{color_quantity(low_items[4].quantity)} | #{low_items[5].department.name}: #{low_items[5].name}: #{color_quantity(low_items[5].quantity)}"
+    end
+
+    def color_quantity(number)
+        if number > 130
+            return number.to_s.colorize(:green)
+        else
+            case number
+                when 80...129
+                    return number.to_s.colorize(:light_yellow)
+                when 30...79
+                    return number.to_s.colorize(:yellow)
+                when 10...30
+                    return number.to_s.colorize(:light_red)
+                when 0...10
+                    return number.to_s.colorize(:red)
+            end
+        end
+    end
+
+    def pry
+        binding.pry
     end
 end
--- lib/inventory_manager.rb
+++ lib/inventory_manager.rb
@@ -1,7 +1,18 @@
 class Inventory_Manager
 
     def initialize
-        
+        puts "Starting Inventory Manager..."
+        @dbc = Database_Controller.new(self)
+    end
+
+    def get_lowest_quantity
+        Item.all.sort do |a,b|
+            a.quantity <=> b.quantity
+        end
+    end
+
+    def pry
+        binding.pry
     end
     
 end
