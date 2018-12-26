fe2d590e6752211e6b29343838e5987fb498b5c6
Jonathan Dust <d24dust@hotmail.com>
2018-12-26 17:16:43 -0500

    Built more commands.

--- lib/concerns/commands.rb
+++ lib/concerns/commands.rb
@@ -1,6 +1,43 @@
 class Interface_Controller
-    module COMMANDS
 
-    end
+    module COMMANDS
+        def home
+            puts "\n\n\n\n\n\n\n\n\n"
+            puts '
+██╗  ██╗ ██████╗ ███╗   ███╗███████╗
+██║  ██║██╔═══██╗████╗ ████║██╔════╝
+███████║██║   ██║██╔████╔██║█████╗  
+██╔══██║██║   ██║██║╚██╔╝██║██╔══╝  
+██║  ██║╚██████╔╝██║ ╚═╝ ██║███████╗
+╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝'
+
+            puts "To return to home at anytime, type \'home\'"
+
+            low_items = @manager.get_lowest_quantity
+
+            puts "\n\nLow Inventory: Order soon!"
+
+            puts "#{low_items[0].department.name}: #{cut_off_after_comma(low_items[0].name)}: #{color_quantity(low_items[0].quantity)} | #{low_items[1].department.name}: #{cut_off_after_comma(low_items[1].name)}: #{color_quantity(low_items[1].quantity)} | #{low_items[2].department.name}: #{cut_off_after_comma(low_items[2].name)}: #{color_quantity(low_items[2].quantity)}"
+            puts "#{low_items[3].department.name}: #{cut_off_after_comma(low_items[3].name)}: #{color_quantity(low_items[3].quantity)} | #{low_items[4].department.name}: #{cut_off_after_comma(low_items[4].name)}: #{color_quantity(low_items[4].quantity)} | #{low_items[5].department.name}: #{cut_off_after_comma(low_items[5].name)}: #{color_quantity(low_items[5].quantity)}"
+            puts "#{low_items[6].department.name}: #{cut_off_after_comma(low_items[6].name)}: #{color_quantity(low_items[6].quantity)} | #{low_items[7].department.name}: #{cut_off_after_comma(low_items[7].name)}: #{color_quantity(low_items[7].quantity)} | #{low_items[8].department.name}: #{cut_off_after_comma(low_items[8].name)}: #{color_quantity(low_items[8].quantity)}"
+            puts "\n\n\n\n\n\n\n\n\n"
+        end
+
+
+        def list
+            @available_commands.each.with_index do |c, i|
+                puts "#{i + 1}. #{c.capitalize}"
+            end
+            puts "Enter number of command or name(in all lowercase)."
+        end
+
+        def all_departments
+            Department.all.each {|d| puts d.name}
+        end
+
+        def focus_department(dept)
+            
+        end
+    end#endof module
     
 end
