4a917430510cba6eba5a0b1736696087874b4fa5
Jonathan Dust <d24dust@hotmail.com>
2018-12-28 16:05:39 -0500

    Created more methods.

--- lib/inventory_manager.rb
+++ lib/inventory_manager.rb
@@ -5,9 +5,21 @@ class Inventory_Manager
         @dbc = Database_Controller.new(self)
     end
 
+    #I.D. items that are soon to be received(date farthest away thats has a received date that is before the order date)
+
     def get_lowest_quantity
         Item.all.sort do |a,b|
             a.quantity <=> b.quantity
+        end
+    end
+
+    def find_object(object_id)
+        ObjectSpace._id2ref(Integer(object_id))
+    end
+
+    def sort_by_name(array)
+        array.sort do |a,b|
+            a.name <=> b.name
         end
     end
 
