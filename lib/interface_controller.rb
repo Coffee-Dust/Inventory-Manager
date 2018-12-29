060d92b19b5cec0bc54af620e8dfb74149b8c4c2
Jonathan Dust <d24dust@hotmail.com>
2018-12-29 16:05:08 -0500

    #Available commands can now handle focus class methods.

--- lib/interface_controller.rb
+++ lib/interface_controller.rb
@@ -26,12 +26,34 @@ class Interface_Controller
             @available_commands.clear
 
             case @command_history.last
-                when "focus_object"
-                    @available_commands = ["back, rename"]
+            when "focus_object"
+                @available_commands = ["back, rename"]
+                
+            else
+                @available_commands = ["view low inventory", "log order", "received order", "find items", "view all departments", "add to database"]
 
-                else
-                    @available_commands = ["view low inventory", "log order", "received order", "find items", "view all departments", "add to database"]
-            end
+                if @command_history.last.is_a? Hash
+
+                    case @command_history.last.keys[0]
+
+                    when "focus_department"
+                        @available_commands = ["view categories", "rename", "back", "delete"]
+                    when "focus_category"
+                        @available_commands = ["rename", "back", "delete"]
+                        #This finds out whether the category has items or not and adds the right method to commands
+                        if cat = @manager.find_object(@command_history.last["focus_category"]).items == nil
+                            @available_commands << "view subcategories"
+
+                        elsif cat.items != nil
+                            @available_commands << "view items"
+                        end
+                    when "focus_subcategory"
+                        @available_commands = ["view items", "rename", "back", "delete"]
+                    else
+                        @available_commands = ["back"]
+                    end
+                end
+            end#endof first case
         end
         @keep_commands = false
     end
@@ -41,7 +63,7 @@ class Interface_Controller
             number = Integer(input)
             sending = @available_commands[number - 1].split(" ").join("_")
             if sending.include? "("
-                
+                #REGEX seperates the input method into sendable data.
                 self.send(sending.scan(/^[a-z _]*/)[0], sending.scan(/\d[^ ()]*/)[0])
                 @command_history << {sending.scan(/^[a-z _]*/)[0]=>sending.scan(/\d[^ ()]*/)[0]}
             else
@@ -68,14 +90,14 @@ class Interface_Controller
             return number.to_s.colorize(:green)
         else
             case number
-                when 80...129
-                    return number.to_s.colorize(:light_yellow)
-                when 30...79
-                    return number.to_s.colorize(:yellow)
-                when 10...30
-                    return number.to_s.colorize(:light_red)
-                when 0...10
-                    return number.to_s.colorize(:red)
+            when 80...129
+                return number.to_s.colorize(:light_yellow)
+            when 30...79
+                return number.to_s.colorize(:yellow)
+            when 10...30
+                return number.to_s.colorize(:light_red)
+            when 0...10
+                return number.to_s.colorize(:red)
             end
         end
     end
