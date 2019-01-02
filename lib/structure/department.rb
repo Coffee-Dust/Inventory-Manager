e23aa2d9cf095caed118b0ebadcad7183108a56e
Jonathan Dust <d24dust@hotmail.com>
2019-01-02 16:23:39 -0500

    Added a #find_category.

--- lib/structure/department.rb
+++ lib/structure/department.rb
@@ -8,5 +8,11 @@ class Department
     def initialize
         self.categories = []
     end
+
+    def find_category(categ)
+        self.categories.detect do |cat|
+            categ == cat
+        end
+    end
     
 end
