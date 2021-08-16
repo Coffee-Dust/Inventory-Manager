bb7dd6ca3ae0b7e28d3a0fc2049b0c3c8ee39198
github username <d24dust@hotmail.com>
2021-08-16 14:26:31 -0400

    fix: String.colorize blank method to fix demo-runtime missing method

--- config/environment.rb
+++ config/environment.rb
@@ -23,5 +23,12 @@ puts '
 
 '
 
+class String
+  def colorize(color)
+    #do nothing so it won't crash during demo runtime
+    return self
+  end
+end
+
 #Chose which environment you want to load.
 # ENVIRONMENTS.select_environment(self)
