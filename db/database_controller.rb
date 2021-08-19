9587734294c9b3b92d17d25cc1877467ff7f95c5
github username <d24dust@hotmail.com>
2021-08-19 18:01:31 -0400

    remove: save on system "at_exit" for demo runtime

--- db/database_controller.rb
+++ db/database_controller.rb
@@ -8,17 +8,17 @@ class Database_Controller
             puts "Successfully loaded database from save."
         end
 
-        base.send("at_exit") do
-            puts "Saving all data to database save file."
-            #BUGFIX: To prevent saving twice in the case of on-exit being called twice.
-            if Thread.current.thread_variable_get("JSON_SAVED_ON_EXIT") == nil && ENV["APP_ENV"] == "DEFAULT"
-                self.save_data_to_json
-                Thread.current.thread_variable_set("JSON_SAVED_ON_EXIT", true)
-                puts "Saved data and exiting. Goodbye \\o/"
-            else
-                puts "Data already saved, no need to save it twice."
-            end
-        end
+        # base.send("at_exit") do
+        #     puts "Saving all data to database save file."
+        #     #BUGFIX: To prevent saving twice in the case of on-exit being called twice.
+        #     if Thread.current.thread_variable_get("JSON_SAVED_ON_EXIT") == nil && ENV["APP_ENV"] == "DEFAULT"
+        #         self.save_data_to_json
+        #         Thread.current.thread_variable_set("JSON_SAVED_ON_EXIT", true)
+        #         puts "Saved data and exiting. Goodbye \\o/"
+        #     else
+        #         puts "Data already saved, no need to save it twice."
+        #     end
+        # end
     end
 
 
